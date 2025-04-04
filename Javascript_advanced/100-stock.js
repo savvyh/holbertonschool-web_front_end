f673b5d1012032988a2106144756dc11a6688a99
savvyh <7277@holbertonstudents.com>
2025-04-04 12:41:35 +0000

    Multiple callbacks

---
+++ Javascript_advanced/100-stock.js
@@ -0,0 +1,27 @@
+let stock = {
+    macbook: 2,
+    iphone: 4
+}
+
+function processPayment(itemName) {
+    stock[itemName]--;
+    console.log(`Payment is being processed for item ${itemName}`);
+}
+
+function processError(itemName) {
+    console.log(`No more ${itemName} in stock`);
+	console.log('Payment is not being processed');
+}
+
+function processOrder(itemName, callbackPayment, callbackError) {
+    console.log(`Verifying the stock of ${itemName}`);
+    if (stock[itemName] > 0)
+    {
+        callbackPayment(itemName);
+    } else {
+        callbackError(itemName);
+    }
+}
+
+let itemChoosen = prompt("Please enter the item you would like to purchase (Macbook, iPhone)");
+processOrder(itemChoosen, processPayment, processError);
