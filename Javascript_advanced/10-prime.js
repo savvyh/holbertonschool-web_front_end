923a3bea1f73f78fa385ec3dbd83cccf4e6897c0
savvyh <7277@holbertonstudents.com>
2025-04-04 11:40:02 +0000

    Execution stack & timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,29 @@
+function countPrimeNumbers() {
+    let count = 0;
+    
+    for (let i = 2; i <= 100; i++)
+    {
+        let isPrime = true;
+        for (let j = 2; j < i; j++)
+        {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+const timeStart = performance.now();
+for (let i = 0; i < 100; i++)
+{
+    countPrimeNumbers();
+}
+const timeEnd = performance.now();
+const timeUsed = timeEnd - timeStart;
+console.log(`Execution time of printing countPrimeNumbers was ${timeUsed} milliseconds.`);
